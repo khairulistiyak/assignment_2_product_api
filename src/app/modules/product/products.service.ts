62cc768d5e8cdf8f5bebba31820a60e44f2db551
khairulistiyak <khairulistiyak@gmail.com>
2024-12-16 15:48:37 -0800

    mongodb save option working

--- src/app/modules/product/products.service.ts
+++ src/app/modules/product/products.service.ts
@@ -1,10 +1,13 @@
 import { TProduct } from './product.interface';
 import { Product } from './product.modules';
 
-const createProductDB = async (product: TProduct) => {
-  const result = await Product.create(product);
+const createProductDB = async (productData: TProduct) => {
+  const product = new Product(productData);
+  const result = await product.save();
   return result;
 };
+
+const product;
 
 export const ProductsServices = {
   createProductDB,
